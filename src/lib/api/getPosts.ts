ef60e2ca215a184e5101c95934d21c1455fb1156
Hyunsoo_kim <red_panda119@naver.com>
2026-05-21 07:57:27 +0900

    feat : getPosts 로직 수정

--- src/lib/api/getPosts.ts
+++ src/lib/api/getPosts.ts
@@ -1,8 +1,11 @@
+import { Post } from "@/types/post";
 import { delay } from "../apiSimulations/delay";
 import { jitter } from "../apiSimulations/jitter";
 import { posts } from "../db/posts";
 
-export const getPosts = async() => {
+export const getPosts = async(id: string) => {
   await delay(jitter());
-  return posts;
+
+  const myPosts = posts.find((post: Post) => post.id === id);
+  return myPosts;
 };
