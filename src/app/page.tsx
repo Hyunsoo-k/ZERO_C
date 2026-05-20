d027ad1ece9aa0cfa82d5d69d221500b21e0077f
Hyunsoo_kim <red_panda119@naver.com>
2026-05-20 10:42:35 +0900

    feat : app 라우터 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+
+import { fetchCompanies } from "@/lib/api";
+import { HomeLayout } from "@/components/layouts/HomeLayout/HomeLayout";
+
 import styles from "./page.module.scss";
 
-export default function Home() {
+export default async function Home() {
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({ queryKey: ["companies"], queryFn: fetchCompanies });
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <HomeLayout />
+        </HydrationBoundary>
       </main>
     </div>
   );
