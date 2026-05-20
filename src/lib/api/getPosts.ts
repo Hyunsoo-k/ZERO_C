d3c1feedd0c6d34475759b6839e49744a6f1ecab
Hyunsoo_kim <red_panda119@naver.com>
2026-05-21 07:45:40 +0900

    style : 함수명 변경

--- src/lib/api/getPosts.ts
+++ src/lib/api/getPosts.ts
@@ -2,7 +2,7 @@ import { delay } from "../apiSimulations/delay";
 import { jitter } from "../apiSimulations/jitter";
 import { posts } from "../db/posts";
 
-export const fetchPosts = async() => {
+export const getPosts = async() => {
   await delay(jitter());
   return posts;
 };
