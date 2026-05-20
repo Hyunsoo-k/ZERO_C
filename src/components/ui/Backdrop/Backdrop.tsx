2f78db6cdc9ff45be08d83eebd34b44b89c0a28d
Hyunsoo_kim <red_panda119@naver.com>
2026-05-20 10:29:14 +0900

    feat : 백드롭 스크롤 방지 추가

--- src/components/ui/Backdrop/Backdrop.tsx
+++ src/components/ui/Backdrop/Backdrop.tsx
@@ -2,12 +2,12 @@
 
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import { useNavDrawerStore } from "@/store/useNavDrawerStore";
+import { useBackdropStore } from "@/store/useBackdropStore";
 
 import styles from "./Backdrop.module.scss";
 
 export const Backdrop = () => {
-  const { isOpen: isNavDrawerOpen, close: closeNavDrawer } = useNavDrawerStore();
+  const { isOpen, close } = useBackdropStore();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -18,12 +18,30 @@ export const Backdrop = () => {
     return () => cancelAnimationFrame(raf);
   }, []);
 
-  if (!isMounted || !isNavDrawerOpen) {
+  useEffect(() => {
+    if (!isMounted) {
+      return;
+    }
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+      document.documentElement.style.overflow = "unset";
+    }
+    
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen, isMounted]);
+
+  if (!isMounted || !isOpen) {
     return null;
   }
   
   return createPortal(
-    <div onMouseDown={closeNavDrawer} className={styles.backdrop} />,
+    <div onMouseDown={close} className={styles.backdrop} style={{ overflow: isOpen ? "hidden" : ""  }} />,
     document.body
   );
 };
