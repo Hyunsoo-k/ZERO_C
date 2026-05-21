821c0f13dfacd5da359c023f2e13ace108fa83d7
Hyunsoo_kim <red_panda119@naver.com>
2026-05-22 05:11:56 +0900

    feat : 알림 모달 구현

--- src/components/modals/AlertModal/AlertModal.tsx
+++ src/components/modals/AlertModal/AlertModal.tsx
@@ -2,13 +2,26 @@
 
 import { createPortal } from "react-dom";
 
+import { useAleryModalStore } from "@/store/useAlertModalStore";
+import { Button } from "@/components/ui/buttons/Button/Button";
+
 import styles from "./AlertModal.module.scss";
 
 export const AlertModal = () => {
+  const { isOpen, message, onClick } = useAleryModalStore();
+
+  if (!isOpen) {
+    return null;
+  }
 
   return createPortal(
     <div className={styles.alertModal}>
-
+      <div className={styles.body}>
+        <p className={styles.message}>{message}</p>
+      </div>
+      <footer className={styles.footer}>
+        <Button content="닫기" onClick={onClick} />
+      </footer>
     </div>,
     document.body
   );
