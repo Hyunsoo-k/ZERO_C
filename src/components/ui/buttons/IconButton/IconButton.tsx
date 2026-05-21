e65353d12802c8f68cc7dd91b4d31b11b186deb8
Hyunsoo_kim <red_panda119@naver.com>
2026-05-22 05:14:50 +0900

    feat : Icon 버튼 구현

--- src/components/ui/buttons/IconButton/IconButton.tsx
+++ src/components/ui/buttons/IconButton/IconButton.tsx
@@ -1,3 +1,4 @@
+import { IoRefreshOutline } from "react-icons/io5";
 import { IoAdd } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 
@@ -5,12 +6,18 @@ import styles from "./IconButton.module.scss";
 
 type Props = {
   content: string;
-  iconType: "search" | "add";
+  iconType: "refresh" | "search" | "add";
   onClick: () => void;
 };
 
 export const IconButton = ({ content, iconType, onClick }: Props) => {
-  const IconComponent = iconType === "search" ? CiSearch : IoAdd;
+  const iconMap = {
+    refresh: IoRefreshOutline,
+    search: CiSearch,
+    add: IoAdd,
+  };
+
+  const IconComponent = iconMap[iconType];
 
   return (
     <button type="button" onClick={onClick} className={styles.iconButton}>
