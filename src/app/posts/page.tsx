494331eec0a61c4bb07cfc3f24ddc1be0a361a20
Hyunsoo_kim <red_panda119@naver.com>
2026-05-21 22:59:31 +0900

    feat : Posts 페이지, 레이아웃 생성

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,27 @@
+import { QueryClient } from "@tanstack/react-query";
+
+import { queryKeys } from "@/queryKeys/queryKeys";
 import { PostsPageLayout } from "@/components/layouts/PostsPageLayout/PostsPageLayout";
 
 import styles from "./page.module.scss";
 
 export default async function PostsPage() {
-  
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 10,
+        retry: 1,
+        refetchOnWindowFocus: false,
+      },
+    }
+  });
+
+  await queryClient.prefetchQuery({ queryKey: queryKeys.posts("c1") });
+
   return (
-    <div className={styles.page}>
+    <main className={styles.page}>
       <PostsPageLayout />
-    </div>
+    </main>
   );
 }
--- src/components/layouts/PostsPageLayout/PostsPageLayout.tsx
+++ src/components/layouts/PostsPageLayout/PostsPageLayout.tsx
@@ -1,25 +1,43 @@
+"use client";
+
+import { Post } from "@/types/post";
 import { useIdStore } from "@/store/useId";
 import { useGetPosts } from "@/hooks/useGetPosts";
+import { useGetCompany } from "@/hooks/useGetCompany";
 import { LoadingSpinner } from "@/components/ui/LoadingSpinner/LoadingSpinner";
+import { PostCard } from "@/components/cards/PostCard/PostCard";
+import { SearchButton } from "@/components/ui/SearchButton/SearchButton";
 
 import styles from "./PostPageLayout.module.scss";
 
 export const PostsPageLayout = () => {
   const { id } = useIdStore();
   const { data: posts } = useGetPosts(id);
+  const { data: company } = useGetCompany(id);
   
-  if (!posts) {
+  if (!posts || !company) {
     return <LoadingSpinner />
   }
 
   return (
-    <div className={styles.PostsPageLayout}>
+    <div className={styles.postsPageLayout}>
       <header className={styles.header}>
-        <h2 className={styles.title}></h2>
+        <h2 className={styles.title}>
+          게시글 목록
+          <span className={styles.name}>(회사명: {company.name})</span>
+        </h2>
+        <p className={styles.description}>탄소 배출 분석글</p>
+        <SearchButton />
       </header>
-      <ul className={styles.list}>
-
-      </ul>
+      <section className={styles.postsSection}>
+        <ul className={styles.list}>
+          {posts.map((post: Post) => (
+            <li key={post.id} className={styles.item}>
+              <PostCard key={post.id} post={post}/>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
