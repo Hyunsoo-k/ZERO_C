bbbd876c39c41c076eae3616bee3ef4f36fbad98
Hyunsoo_kim <red_panda119@naver.com>
2026-05-21 22:59:53 +0900

    feat : postCard 컴포넌트 생성

--- src/components/cards/PostCard/PostCard.tsx
+++ src/components/cards/PostCard/PostCard.tsx
@@ -1,14 +1,35 @@
+import { Post } from "@/types/post";
+
 import styles from "./PostCard.module.scss";
 
-export const PostCard = () => {
+type Props = {
+  post: Post
+};
+
+export const PostCard = ({ post }: Props) => {
+  const { title, content, dateTime } = post;
+
+  const handleButtonClick = () => {
+
+  };
+
   return (
     <article className={styles.postCard}>
       <header className={styles.header}>
-        <h3 className={styles.title}></h3>
+        <h3 className={styles.title}>{title}</h3>
+        <small className={styles.date}>{dateTime}</small>
       </header>
-      <div className={styles.body}></div>
+      <div className={styles.body}>
+        <p className={styles.content}>{content}</p>
+      </div>
       <footer className={styles.footer}>
-        
+        <button
+          type="button"
+          onClick={handleButtonClick}
+          className={styles.openButton}
+        >
+          열기
+        </button>
       </footer>
     </article>
   );
