4b837e77046f930b3650d84978a857593e51775e
Hyunsoo_kim <red_panda119@naver.com>
2026-05-21 09:42:44 +0900

    feat : 게시글 페이지 컴포넌트 렌더링

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,12 @@
+import { PostsPageLayout } from "@/components/layouts/PostsPageLayout/PostsPageLayout";
+
 import styles from "./page.module.scss";
 
 export default async function PostsPage() {
   
   return (
     <div className={styles.page}>
-
+      <PostsPageLayout />
     </div>
   );
 }
