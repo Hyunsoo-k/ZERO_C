532a9ad3853707143541a582d4a0cd5160496a38
Hyunsoo_kim <red_panda119@naver.com>
2026-05-22 03:01:04 +0900

    feat : IconButton 컴포넌트 삽입

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { CiMenuBurger } from "react-icons/ci";
 
 import { useNavDrawerStore } from "@/store/useNavDrawerStore";
-import { CreateButton } from "@/components/ui/CreateButton/CreateButton";
+import { IconButton } from "@/components/ui/buttons/IconButton/IconButton";
 
 import styles from "./Header.module.scss";
 
@@ -15,13 +15,17 @@ export const Header = () => {
     openNavDrawer();
   };
 
+  const handleAddButtonClick = () => {
+
+  };
+
   return (
     <header className={styles["header"]}>
       <button type="button" onClick={handleBurgerClick} className={styles.navDrawerButton}>
         <CiMenuBurger  className={styles.burger} />
       </button>
       <Link href="/"><h1 className={styles.brand}>ZERO_C</h1></Link>
-      <CreateButton />
+      <IconButton content="기록 추가" iconType="add" onClick={handleAddButtonClick} />
     </header>
   );
 };
